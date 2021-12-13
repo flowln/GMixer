b0d916ed1680c7151e8ca8e44364d53b45c2a3d7
Thiago <thiagodonato300@gmail.com>
2021-12-13 00:09:54 -0300

    PipelineCreation: Add FileChooserDialog when importing from a file

--- src/backend/PipelineCreation.cpp
+++ src/backend/PipelineCreation.cpp
@@ -3,6 +3,7 @@
 
 #include <gtkmm/application.h>
 #include <gtkmm/dialog.h>
+#include <gtkmm/filechoosernative.h>
 #include <gtkmm/label.h>
 #include <gtkmm/entry.h>
 #include <gtkmm/boxlayout.h>
@@ -33,7 +34,7 @@ void PipelineFactory::createPipeline()
 
     auto create_button = Gtk::make_managed<Gtk::Button>("Create");
     create_button->set_sensitive(false);
-    dialog->add_action_widget(*create_button, GTK_RESPONSE_OK);
+    dialog->add_action_widget(*create_button, GTK_RESPONSE_ACCEPT);
 
     name_entry->signal_changed().connect(
         [create_button, name_entry](){
@@ -45,7 +46,7 @@ void PipelineFactory::createPipeline()
 
     dialog->signal_response().connect(
         [dialog, name_entry](int response_id){
-            if(response_id == GTK_RESPONSE_OK){
+            if(response_id == GTK_RESPONSE_ACCEPT){
                 auto name = name_entry->get_text();
                 PipelineSelector::getInstance()->addPipeline(Glib::make_refptr_for_instance(new Pipeline(name)));
             }
@@ -59,5 +60,23 @@ void PipelineFactory::createPipeline()
 
 void PipelineFactory::createPipelineFromFile()
 {
-    PipelineSelector::getInstance()->addPipeline(Glib::make_refptr_for_instance(Pipeline::createFromString("abc", "audiotestsrc ! fakesink")));
+    if(!s_main_window)
+        return;
+   
+    auto dialog = Gtk::FileChooserNative::create("Please select the pipeline to import ({name}.xml).", *s_main_window, Gtk::FileChooser::Action::OPEN, "Select", "Cancel");
+    dialog->set_modal(true);
+    dialog->set_transient_for(*s_main_window);
+
+    dialog->signal_response().connect(
+        [dialog](int id){
+            if(id == GTK_RESPONSE_ACCEPT){
+                auto file = dialog->get_file();
+                auto data = parseFile(file);
+    
+                PipelineSelector::getInstance()->addPipeline(Glib::make_refptr_for_instance(Pipeline::createFromString(&data.first, &data.second)));
+            }
+        });
+
+    dialog->show();
 }
+
