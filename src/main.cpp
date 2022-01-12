ec6403a4b7117bcd93bc2dfe16eaee7070008914
flow <thiagodonato300@gmail.com>
2022-01-12 16:45:31 -0300

    Refactor: Add 'GtkClient'
    Makes it possible to implement other frontends other than the Gtk one
    (e.g. a command-line one).

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,9 @@
-#include <gtkmm/application.h>
 #include <gst/gst.h>
 
-#include "client/gtk/MainWindow.h"
+#include "client/GtkClient.h"
 
 int main (int argc, char *argv[])
 {
     gst_init (&argc, &argv);
-
-    auto app = Gtk::Application::create("org.flowln.GMixer");
-    app->register_application();
-
-    return app->make_window_and_run<MainWindow>(argc, argv);
+    return GtkClient::runClient("org.flowln.GMixer", argc, argv);
 }
