29f3f433f091d5cf5b3f74f697bb14f327e0d981
flow <flowlnlnln@gmail.com>
2023-04-09 11:06:12 -0300

    fix(Logging): only color the tag
    Signed-off-by: flow <flowlnlnln@gmail.com>

--- src/client/cmd/CommandLineLogger.cpp
+++ src/client/cmd/CommandLineLogger.cpp
@@ -4,7 +4,9 @@
 
 CommandLineLogger::CommandLineLogger(Logger::LoggingLevel level) : Logger(level) {}
 
-static const char* end_message   = "\033[0;39m\n";
+static const char* end_message   = "\n";
+static const char* end_color     = "\033[0;39m";
+
 static const char* debug_color   = "\033[35m";
 static const char* info_color    = "\033[34m";
 static const char* warning_color = "\033[33m";
@@ -15,7 +17,7 @@ bool CommandLineLogger::sendDebug(std::string&& message) const
     if (!Logger::sendDebug(""))
         return false;
 
-    printf("%s[DEBUG] %s %s", debug_color, message.c_str(), end_message);
+    printf("%s[DEBUG]%s %s %s", debug_color, end_color, message.c_str(), end_message);
     return true;
 }
 
@@ -24,7 +26,7 @@ bool CommandLineLogger::sendInfo(std::string&& message) const
     if (!Logger::sendInfo(""))
         return false;
 
-    printf("%s[INFO] %s %s", info_color, message.c_str(), end_message);
+    printf("%s[INFO]%s %s %s", info_color, end_color, message.c_str(), end_message);
     return true;
 }
 
@@ -33,7 +35,7 @@ bool CommandLineLogger::sendWarning(std::string&& message) const
     if (!Logger::sendWarning(""))
         return false;
 
-    printf("%s[WARNING] %s %s", warning_color, message.c_str(), end_message);
+    printf("%s[WARNING]%s %s %s", warning_color, end_color, message.c_str(), end_message);
     return true;
 }
 
@@ -43,9 +45,9 @@ bool CommandLineLogger::sendError(std::string&& message, std::exception* exc) co
         return false;
 
     if (exc) {
-        printf("%s[ERROR] %s:\n\t--> %s %s", error_color, message.c_str(), exc->what(), end_message);
+        printf("%s[ERROR]%s %s:\n\t--> %s %s", error_color, end_color, message.c_str(), exc->what(), end_message);
     } else {
-        printf("%s[ERROR] %s %s", error_color, message.c_str(), end_message);
+        printf("%s[ERROR]%s %s %s", error_color, end_color, message.c_str(), end_message);
     }
     return true;
 }
